412faa8dceda1b77f91624cbc646cfc63be3ca51
delta <amin138400138400@gmail.com>
2025-11-26 20:54:20 +0330

    Change in Templates

--- Video/videocall/urls.py
+++ Video/videocall/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lobby/', views.video_chat_lobby, name='lobby'),
+    # نام را از 'lobby' به 'video_chat_lobby' تغییر دهید تا با فایل‌های HTML هماهنگ شود
+    path('lobby/', views.video_chat_lobby, name='video_chat_lobby'),
     path('add_coins/', views.add_coins_view, name='add_coins'),
 ]
